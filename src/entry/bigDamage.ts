9fdd70b23d597b5cc0af779a4aa3b386232bdb28
Tanimodori <unknowner2014@gmail.com>
2025-03-24 13:48:12 +0800

    feat: bigDamage

--- src/entry/bigDamage.ts
+++ src/entry/bigDamage.ts
@@ -2,7 +2,7 @@ globalThis.pinMatchesFightEvent = (event, _fight) => {
   if (event.type === 'damage' && event.targetDisposition === 'friendly') {
     const unmitigated = event.unmitigatedAmount;
     const maxhp = event.targetResources?.maxHitPoints;
-    if (maxhp && unmitigated >= maxhp / 0.95) {
+    if (maxhp && unmitigated * 1.1 >= maxhp) {
       return true;
     }
   }
